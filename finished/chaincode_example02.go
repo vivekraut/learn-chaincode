233ee6dede005727ea1dea1f6bbd874c8e462ea3
vivekraut <vivek.raut@accenture.com>
2016-10-26 12:28:51 +0530

    Updated write method

--- finished/chaincode_example02.go
+++ finished/chaincode_example02.go
@@ -98,6 +98,22 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 			      fmt.Println("")
 				  value = args[i]
 			 }
+			 
+			 //check if the state exists. If not initialize the state
+			 Avalbytes, err := stub.GetState(key)
+			if err != nil {
+				jsonResp := "{\"Error\":\"Failed to get state for " + key + "\"}"
+				return nil, errors.New(jsonResp)
+			}
+		
+			if Avalbytes == nil {
+				Avalbytes = []byte("0")
+				err = stub.PutState(key, Avalbytes)
+				
+				//jsonResp := "{\"Error\":\"Nil amount for " + key + "\"}"
+				//return nil, errors.New(jsonResp)
+			}
+			 
 		     err = stub.PutState(key, []byte(value))  //write the variable into the chaincode state
 			 if err != nil {
 				return nil, err
