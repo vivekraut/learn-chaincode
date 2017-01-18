724d4f2bdb1041f64c800bae478af6844c85d47e
vivekraut <vivek.raut@accenture.com>
2017-01-18 10:23:00 +0530

    Updated error message

--- finished/error_json.go
+++ finished/error_json.go
@@ -1,2 +1,24 @@
+package main
 
+
+const (
+  //Utility ComponentCode = "Utility"
+  Logging ComponentCode = "Logging"
+  //Peer ComponentCode = "Peer"
+  
+)
+
+const (
+  LoggingUnknownError ReasonCode = "LoggingUnknownError"
+  
+)
+
+const errorMapping string = `
+{
+"Logging":
+  {"LoggingUnknownError" :
+    {"en":"An unknown error has occured"}
+
+  }
+}
 
